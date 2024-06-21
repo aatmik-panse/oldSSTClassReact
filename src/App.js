f6fc286499c63721fdf7b3a7fdf13a4595d2eae9
aatmik-panse <aatmik.23bcs10082@ms.sst.scaler.com>
2024-06-21 10:20:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import "./App.css";
+import { a, b } from "./components/Products/Products";
+import Products from "./components/Products/Products";
+import { useState } from "react";
+import CartContext from "./context/CartContext";
+function App() {
+  // state variable
+  // inc
+  // dec
+  let [cart, setCart] = useState({});
+  function increaseQuantity(product) {
+    const newCart = { ...cart };
+    // if(cart[product.id])
+    if (!newCart[product.id]) {
+      newCart[product.id] = {
+        ...product,
+        quantity: 0,
+      };
+    }
+    newCart[product.id].quantity += 1;
+    console.log(newCart);
+    setCart(newCart);
+  }
+
+  function decreaseQuantity(product) {
+    const newCart = { ...cart };
+    if (!newCart[product.id]) return;
+    newCart[product.id].quantity -= 1;
+    if (newCart[product.id].quantity <= 0) {
+      delete newCart[product.id];
+    }
+    setCart(newCart);
+  }
+
+  console.log(a, b);
+  return (
+    <CartContext.Provider value={{ cart, increaseQuantity, decreaseQuantity }}>
+      <div className="App">
+        <Products
+          cart={cart}
+          increaseQuantity={increaseQuantity}
+          decreaseQuantity={decreaseQuantity}
+        />
+      </div>
+    </CartContext.Provider>
+  );
+}
+
+export default App;
